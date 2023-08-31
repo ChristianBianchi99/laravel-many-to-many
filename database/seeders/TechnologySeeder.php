0ec6cffc360e798505ae7d3bd50d2f506f9c5477
ChristianBianchi99 <bianchi848@gmail.com>
2023-08-31 15:48:47 +0200

    create technology table

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['HTML', 'CSS', 'Javascript', 'JQuery', 'VueJS', 'Laravel'];
+
+        foreach($technologies as $item){
+
+            $technology = new Technology();
+            $technology->name = $item;
+            $technology->slug = Str::slug($item, '-');
+            $technology->save();
+        }
+    }
+}
